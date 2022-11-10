05e7ea570fe5a6b46dc88d42fbd1d0fa9d94042e
Leon Chu <Ljchu87@gmail.com>
2022-11-10 10:17:55 -0600

    Setup and import routes for about, contact, home, projects, and resume sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes> 
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='resume' element={<Resume />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
