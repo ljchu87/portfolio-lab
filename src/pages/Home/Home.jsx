82cf9f780d49a69e9431cda9d3ddb854dfe55650
Leon Chu <Ljchu87@gmail.com>
2022-11-23 17:47:39 -0600

    Add typewriter effect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from './Home.module.css'
+import TypeWriter from 'typewriter-effect'
 
 const Home = () => {
   return (
@@ -7,6 +8,23 @@ const Home = () => {
       <div className={styles.hero}>
         <h2>Hi there! My name is</h2>
         <h1>Leon Chu</h1>
+        <div className={styles.app}>
+          <TypeWriter 
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("I'm a software engineer.")
+                .pauseFor(500)
+                .deleteAll()
+                .typeString("I'm a dog dad.")
+                .pauseFor(500)
+                .deleteAll()
+                .typeString("I'm a fitness enthusiast.")
+                .pauseFor(500)
+                .deleteAll()
+                .start()
+            }}
+          />
+        </div>
       </div>
     </div>
     </>
