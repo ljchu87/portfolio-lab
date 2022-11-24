12ef2e1e29a8b1d9e0ca5c974baf253d084d9ff3
Leon Chu <Ljchu87@gmail.com>
2022-11-24 08:39:12 -0600

    Fix resume view

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <>
       <h1 className={styles.headerAbout}>Resume</h1>
       <h2 className={styles.headerAbout2}>Checkout my resume below to stay up-to-date with my work!</h2>
-      <iframe className={styles.myPDF} src="Leon_Chu_resume.pdf" frameborder="0"></iframe>
+      <iframe className={styles.myPDF} src="Leon_Chu_resume.pdf" frameborder="0" title="myResume"></iframe>
     </>
   )
 }
