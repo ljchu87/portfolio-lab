a68e60a0910f5cbd0bd9786460a60a5ab6f2f00c
Leon Chu <Ljchu87@gmail.com>
2022-11-26 10:35:30 -0600

    Update subheader size

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <>
     <div className={styles.resumeContainer}>
       <h1 className={styles.headerAbout}>Resume</h1>
-      <h2 className={styles.headerAbout2}>Checkout my resume below to stay up-to-date with my work!</h2>
+      <h4 className={styles.headerAbout2}>Checkout my resume below to stay up-to-date with my work!</h4>
       <iframe className={styles.myPDF} src="Leon_Chu_resume.pdf" frameborder="0" title="myResume"></iframe>
     </div>
     </>
