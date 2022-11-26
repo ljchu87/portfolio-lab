233c7bf861320dd654369efcb12d0b56189ff9f8
Leon Chu <Ljchu87@gmail.com>
2022-11-26 14:50:41 -0600

    Add hover effect on social icons in contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -20,13 +20,13 @@ const Contact = () => {
       <h4 className={styles.contactSubHeader}>Send me an email and let's connect!</h4>
       <div className={styles.socialContainer}>
         <a href="https://www.linkedin.com/in/leon-chu/">
-          <img src="/images/icons8-linkedin.svg" alt="LinkedIn Logo" />
+          <img src="/images/icons8-linkedin.svg" alt="LinkedIn Logo" class={styles.hvrPulse} />
         </a>
         <a href="https://twitter.com/ljchu">
-          <img src="/images/icons8-twitter.svg" alt="Twitter Logo" />
+          <img src="/images/icons8-twitter.svg" alt="Twitter Logo" class={styles.hvrPulse} />
         </a>
         <a href="https://github.com/ljchu87">
-          <img src="/images/icons8-github-squared.svg" alt="GitHub Logo" />
+          <img src="/images/icons8-github-squared.svg" alt="GitHub Logo" class={styles.hvrPulse} />
         </a>
       </div>
       <div className='container'>
