9ad5f6e9f423a1a2c54eac111ae73ffaefe5c49c
Leon Chu <Ljchu87@gmail.com>
2022-11-26 22:01:47 -0600

    Remove border git add . clear

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.hero}>
         <h2 className={styles.header1}>Hello, my name is</h2>
-        <h1 className={styles.linearWipe}>Leon Chu</h1>
+        <h1 className={styles.linearWipe}>LEON CHU</h1>
         <div className={styles.app}>
         <TypeWriter 
             onInit={(typewriter) => {
