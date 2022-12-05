e0890ec86072a322871f8cca20ea7c58cc1822e8
Leon Chu <Ljchu87@gmail.com>
2022-12-05 10:31:02 -0600

    Add socials on about me page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,21 @@ const About = () => {
   return (
     <>
       <h1 className={styles.linearWipe}>About</h1>
+      <h4 className={styles.aboutHeader2}>Get to know me a little better.</h4>
+      <div className={styles.socialContainer}>
+        <a href="https://www.linkedin.com/in/leon-chu/" target="_blank" rel="noopener noreferrer">
+          <img src="/images/icons8-linkedin.svg" alt="LinkedIn Logo" class={styles.hvrPulse} />
+        </a>
+        <a href="https://twitter.com/ljchu" target="_blank" rel="noopener noreferrer">
+          <img src="/images/icons8-twitter.svg" alt="Twitter Logo" class={styles.hvrPulse} />
+        </a>
+        <a href="https://github.com/ljchu87" target="_blank" rel="noopener noreferrer">
+          <img src="/images/icons8-github-squared.svg" alt="GitHub Logo" class={styles.hvrPulse} />
+        </a>
+        <a href="/contact" target="_blank" rel="noopener noreferrer">
+          <img src="/images/icons8-mail.svg" alt="Email Logo" class={styles.hvrPulse} />
+        </a>
+      </div>
       <div className={styles.about}>
         <img src="images/Leon.jpg" alt="Leon" className='profile' />
           <div>
